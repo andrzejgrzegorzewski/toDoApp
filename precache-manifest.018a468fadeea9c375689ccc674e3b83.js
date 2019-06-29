c777ca96587d6b3ce6812590b555a64921eb5802
Andrzej Grzegorzewski <andrzej.grzegorzewski@gmail.com>
2019-06-30 00:02:36 +0200

    Updates

---
+++ precache-manifest.018a468fadeea9c375689ccc674e3b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d86cebe311abab343296",
+    "url": "/toDoApp/static/js/runtime~main.d86cebe3.js"
+  },
+  {
+    "revision": "bda681271c3e0a11bb37",
+    "url": "/toDoApp/static/js/main.bda68127.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/toDoApp/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "bda681271c3e0a11bb37",
+    "url": "/toDoApp/static/css/main.bd120543.chunk.css"
+  },
+  {
+    "revision": "5716363fc52bd8c5d339eb8c13d56687",
+    "url": "/toDoApp/index.html"
+  }
+];
